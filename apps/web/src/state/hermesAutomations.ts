98ce4bb0cc21537a8b804193670c3d4c6f04380d
Brandon Phillips <bpdev97@gmail.com>
2026-07-17 12:14:55 -0400

    Improve Hermes tool call details

--- apps/web/src/state/hermesAutomations.ts
+++
@@ -1,6 +0,0 @@
-import { createHermesAutomationEnvironmentAtoms } from "@t3tools/client-runtime/state/hermes-automations";
-
-import { connectionAtomRuntime } from "../connection/runtime";
-
-export const hermesAutomationEnvironment =
-  createHermesAutomationEnvironmentAtoms(connectionAtomRuntime);
